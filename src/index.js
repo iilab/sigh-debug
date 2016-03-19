ba8082a964da19ced71bc2683f31e12ea04d2efa
Jun Matsushita <jun@iilab.org>
2016-03-19 19:15:53 +0100

    Log all events

--- src/index.js
+++ src/index.js
@@ -5,14 +5,8 @@ import { mapEvents } from 'sigh-core/lib/stream'
 
 export default function(op, opts = {}) {
   return mapEvents(op.stream, function(event) {
-    if (event.type !== 'add' && event.type !== 'change')
-      return event
 
     console.log(event.type + ": " + event.path)
-
-    // if (event.fileType !== 'relevantType') return event
-    // TODO: alter event here or return a new event
-    // event.changeFileSuffix('newSuffix')
 
     return event
   })
